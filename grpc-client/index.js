964b51911bee897080008aa67117157c46595dbc
TuanMadison <yentth237@gmail.com>
2022-08-03 14:34:30 +0700

    feat: add grpc for max number

--- grpc-client/index.js
+++ grpc-client/index.js
@@ -3,27 +3,46 @@ const grpc = require("@grpc/grpc-js");
 var protoLoader = require("@grpc/proto-loader");
 const app = express();
 
-const packageDefinition = protoLoader.loadSync('../notes.proto');
-const newsProto = grpc.loadPackageDefinition(packageDefinition);
-
+const packageDefinition = protoLoader.loadSync('../protos/notes.proto');
+const maxDefinition = protoLoader.loadSync('../protos/maximum.proto');
 const NoteService = grpc.loadPackageDefinition(packageDefinition).NoteService;
+const MaxService = grpc.loadPackageDefinition(maxDefinition).MaxService;
 
 // @ts-ignore
-const client = new NoteService(
+const noteClient = new NoteService(
   "localhost:50051",
   grpc.credentials.createInsecure()
 );
 
+// @ts-ignore
+const maxClient = new MaxService(
+  "localhost:50051",
+  grpc.credentials.createInsecure()
+);
+
+
 app.get('/', (req, res) => {
-  client.list({}, (error, data) => {
+  noteClient.list({}, (error, data) => {
     if (error) {
       return res.send({
         error: error
       });
     }
+    console.log(data);
+
     res.send({
       data
     });
+  });
+});
+
+app.get('/max', (req, res) => {
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  maxClient.maxArray({ numbers }, (error, data) => {
+    if (error) {
+      return res.send({ error: error });
+    }
+    res.send({ data });
   });
 });
 
--- grpc-server/index.js
+++ grpc-server/index.js
@@ -1,8 +1,11 @@
 const grpc = require('@grpc/grpc-js');
+const { log } = require('@grpc/grpc-js/build/src/logging');
 const protoLoader = require('@grpc/proto-loader');
 const server = new grpc.Server();
-const packageDefinition = protoLoader.loadSync('../notes.proto');
+const packageDefinition = protoLoader.loadSync('../protos/notes.proto');
+const maxDefinition = protoLoader.loadSync('../protos/maximum.proto');
 const newsProto = grpc.loadPackageDefinition(packageDefinition);
+const maxProto = grpc.loadPackageDefinition(maxDefinition);
 
 const notes = {
   notes: [
@@ -16,6 +19,24 @@ server.addService(newsProto.NoteService.service, {
     callback(null, notes)
   },
 })
+
+
+// @ts-ignore
+server.addService(maxProto.MaxService.service, {
+  maxArray: (call, callback) => {
+    const { numbers } = call.request
+    console.log(call.request);
+    let maxNumber = 0;
+    if (numbers && numbers.length) {
+      for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > maxNumber) {
+          maxNumber = numbers[i];
+        }
+      }
+    }
+    callback(null, { number: maxNumber });
+  }
+});
 
 server.bindAsync('127.0.0.1:50051', grpc.ServerCredentials.createInsecure(), (err, port) => {
   console.log(`Server running at http://127.0.0.1:${port}`)
